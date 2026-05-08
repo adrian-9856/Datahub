c73a244da1577904d553faa7baa5e8a0dede662a
adrian-9856 <adrian.torres9856@gmail.com>
2026-05-08 10:59:04 -0600

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@
 // 3. Copia la URL (https://script.google.com/macros/s/[ID]/exec)
 // 4. Pégala abajo reemplazando "YOUR_ID_HERE"
 
-window.API_URL = 'https://script.google.com/macros/s/YOUR_ID_HERE/exec';
+window.API_URL = 'https://script.google.com/macros/s/AKfycbxJxzrgtkiEY4MbxsvOx8tdJ-6BjGPJgHy25mcYWJQbEXubU1wZCboGUkhYdMK-Y6vJNA/exec';
 
 // Eso es todo lo que necesitas configurar 👆
